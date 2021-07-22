f73bac0541cd3f29cfca3323c862b5e244d860e6
Farid <44942752+FiNuri@users.noreply.github.com>
2021-07-22 14:58:01 +0400

    now work on multiple screen

--- src/com/farid/fmatrixscreen/Main.java
+++ src/com/farid/fmatrixscreen/Main.java
@@ -1,9 +1,16 @@
 package com.farid.fmatrixscreen;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(MatrixWindow::new);
+        SwingUtilities.invokeLater(() -> {
+            GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            GraphicsDevice[] graphicsDevices = graphicsEnvironment.getScreenDevices();
+            for (GraphicsDevice graphicsDevice: graphicsDevices) {
+                graphicsDevice.setFullScreenWindow(new MatrixWindow());
+            }
+        });
     }
 }
