8dc89ec46e9faef0fd5f0a95bba134dd5e9ee503
Farid <44942752+FiNuri@users.noreply.github.com>
2021-07-23 10:26:25 +0400

    added color gradient depending on distance

--- src/com/farid/fmatrixscreen/ColorHolder.java
+++ src/com/farid/fmatrixscreen/ColorHolder.java
@@ -3,10 +3,15 @@ package com.farid.fmatrixscreen;
 import java.awt.*;
 
 public class ColorHolder {
-    private Color[] colors;
+    private final Color[] colorsZ1;
+    private final Color[] colorsZ2;
+    private final Color[] colorsZ3;
+    private final Color[] colorsZ4;
+    private final Color[] colorsZ5;
+    private final Color[] colorsZ6;
 
     public ColorHolder() {
-        this.colors = new Color[]{
+        this.colorsZ1 = new Color[]{
                 new Color(233, 255, 236),
                 new Color(182, 250, 193),
                 new Color(59, 255, 115),
@@ -16,9 +21,79 @@ public class ColorHolder {
                 new Color(1, 35, 1),
                 new Color(1, 12, 1)
         };
+        this.colorsZ2 = new Color[]{
+                new Color(233, 255, 236, 230),
+                new Color(182, 250, 193, 230),
+                new Color(59, 255, 115, 230),
+                new Color(0, 255, 65, 230),
+                new Color(0, 143, 17, 230),
+                new Color(0, 71, 12, 230),
+                new Color(1, 35, 1, 230),
+                new Color(1, 12, 1, 230)
+        };
+        this.colorsZ3 = new Color[]{
+                new Color(233, 255, 236, 153),
+                new Color(182, 250, 193, 153),
+                new Color(59, 255, 115, 153),
+                new Color(0, 255, 65, 153),
+                new Color(0, 143, 17, 153),
+                new Color(0, 71, 12, 153),
+                new Color(1, 35, 1, 153),
+                new Color(1, 12, 1, 153)
+        };
+        this.colorsZ4 = new Color[]{
+                new Color(233, 255, 236, 128),
+                new Color(182, 250, 193, 128),
+                new Color(59, 255, 115, 128),
+                new Color(0, 255, 65, 128),
+                new Color(0, 143, 17, 128),
+                new Color(0, 71, 12, 128),
+                new Color(1, 35, 1, 128),
+                new Color(1, 12, 1, 128)
+        };
+        this.colorsZ5 = new Color[]{
+                new Color(233, 255, 236, 153),
+                new Color(182, 250, 193, 153),
+                new Color(59, 255, 115, 153),
+                new Color(0, 255, 65, 153),
+                new Color(0, 143, 17, 153),
+                new Color(0, 71, 12, 153),
+                new Color(1, 35, 1, 153),
+                new Color(1, 12, 1, 153)
+        };
+        this.colorsZ6 = new Color[]{
+                new Color(233, 255, 236, 128),
+                new Color(182, 250, 193, 128),
+                new Color(59, 255, 115, 128),
+                new Color(0, 255, 65, 128),
+                new Color(0, 143, 17, 128),
+                new Color(0, 71, 12, 128),
+                new Color(1, 35, 1, 128),
+                new Color(1, 12, 1, 128)
+        };
     }
 
-    public Color[] getColors() {
-        return colors;
+    public Color[] getColorsZ1() {
+        return colorsZ1;
+    }
+
+    public Color[] getColorsZ2() {
+        return colorsZ2;
+    }
+
+    public Color[] getColorsZ3() {
+        return colorsZ3;
+    }
+
+    public Color[] getColorsZ4() {
+        return colorsZ4;
+    }
+
+    public Color[] getColorsZ5() {
+        return colorsZ5;
+    }
+
+    public Color[] getColorsZ6() {
+        return colorsZ6;
     }
 }
